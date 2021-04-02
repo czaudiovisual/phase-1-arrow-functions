02154052afab4b6ce44a880ca9793979054bed09
Christian Bermeo <czaudiovisual@hotmail.com>
2021-04-01 22:14:36 -0400

    commit tree c5e7188de594771e5ff61e673c563eb190159579

---
+++ index.js
@@ -0,0 +1,34 @@
+// function divide(){
+//     const division = 2000 / 100;
+//     return division;
+// }
+// divide();
+
+function divide() {
+    return 2000 / 100;
+}
+divide();
+
+
+// function square(x) {
+//     const value = (x) => x * 2;
+//     return x * 2;
+// }
+
+function square(x) {
+    return x * 2;
+}
+square(2);
+
+
+
+// function add (a,b) {
+//     const sum = (a,b) => a + b 
+//     return a + b;
+// }
+// sum (4,3);
+
+function add (a,b) {
+    return a + b;
+}
+add(4,3);
